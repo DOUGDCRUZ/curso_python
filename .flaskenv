5a071959272945c0759433196fed88e6aea324b7
dougdcruz <dougdcruz@gmail.com>
2022-05-24 16:28:04 +0000

    aula 1

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP = app.py
+FLASK_ENV = development # define o ambiente production ou development
+FLASK_RUN_HOST = 0.0.0.0 # ao invés de rodar no 0.0.0.1 (FLASK_ENV = production)
---
+++ app.py
@@ -0,0 +1,35 @@
+'''
+No terminal:
+* Instalar a biblioteca/módulo: pip install flask
+* Verificar a versão: flask --version
+* Se necessário, atualizar: pip install --upgrade flask
+* pip freeze > requirements.txt # criar arquivo requirements com todas as saídas do pip freeze
+'''
+
+from flask import Flask
+
+app = Flask('Hello')   #Flask é uma classe
+
+@app.route('/hello')    #criar uma rota => página WEB com nome hello
+def hello():
+    return 'Olá Douglas!'
+@app.route('/tchau')    #criar uma rota => página WEB com nome hello
+def tchau():
+    return 'tchau'
+
+'''
+Para atualizar automaticamente as alterações no código faça as alteraçõea abaixo
+No terminal:
+pip install python-dotenv # envia as varíaveis de configuração contidas no arquivo .flaskenv
+pip freeze > requirements.txt # criar o arquivo com todas as bibliotecas instaladas durante o desenvolvimento
+echo $USER # variável do ambiente($) => nome do usuário
+echo $HOME
+echo $PATH # o caminho de todos os arquivos executáveis da máquina
+
+Criar um novo arquivo de configuração com nome (começa com ponto porque ele é oculto): .flaskenv
+
+
+git add .
+git commit -m 'aula 1'
+git push
+'''
